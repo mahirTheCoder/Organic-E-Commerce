cabff4b4802869c4ad3077b2c492136013e0d72f
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-11-16 23:49:21 +0600

    e commerce

---
+++ src/app/Components/topbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <section id="topbar">
+      <div className="w-full h-10 bg-red-900 text-white flex justify-center items-center">
+        jusv
+      </div>
+    </section>
+  );
+};
+
+export default Topbar;
